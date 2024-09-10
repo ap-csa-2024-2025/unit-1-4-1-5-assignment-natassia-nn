d9c8d5d15befcc42d33f0cb630677f1829401f98
natassia-nn <natassianguyen@gmail.com>
2024-09-10 10:16:11 -0700

    made changes

--- Problem0.java
+++ Problem0.java
@@ -2,6 +2,11 @@ public class Problem0
 {
   public static void main(String[] args)
   {
-    // insert code here
+    double value = 2;
+    System.out.print(value);
+
+    int number = 2.01;
+    System.out.print(number);
   }
 }
+ // can't store doubles into int, but you can store int into double
--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,9 @@
-
+public class Problem1
+{
+    public static void main(String[] args)
+    {
+       int value = 2;
+       int value++;
+       
+    }
+}
